c7812495c9ab7021f37a51fd779f538ef6fae388
minoli <minoli@liveroom.xyz>
2020-07-25 20:48:14 +0530

    second commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const [data, setData] = useState({ hits: [] });
+  const [query, setQuery] = useState('redux');
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=redux',);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setIsError(false);
+      try {
+        const result = await axios(url);
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    }
+    fetchData();
+  }, [url]);
+
+  return (
+    <Fragment>
+      <form onSubmit={event => {
+        setUrl(`http://hn.algolia.com/api/v1/search?query=${query}`);
+        event.preventDefault();
+      }}>
+        <input
+          type="text"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+        />
+        <button type="submit">Search</button>
+      </form>
+      {isError && <div>Something went wrong ...</div>}
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+          <ul>
+            {data.hits.map(item => (
+              <li key={item.objectID}>
+                <a href={item.url}>{item.title}</a>
+              </li>
+            ))}
+          </ul>
+        )}
+    </Fragment>
+  );
+}
+
+export default App;
